be00564ca3e284c62801d51f4b9ffec5c92faf47
Ruben Abarca Navarro <dev@espaciofuturo.io>
2024-10-16 08:30:57 -0600

    chore: update theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,14 +49,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
+        {/* <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
           {children}
-        </ThemeProvider>
+        </ThemeProvider> */}
+        {children}
       </body>
     </html>
   );
